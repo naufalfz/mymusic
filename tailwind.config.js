a40bf5e9a210466faabd568dadf22934f578e044
naufalfz <naufal.farisi@gmail.com>
2024-05-19 21:16:16 +0700

    halaman login dan register

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        header: '#3D3759',
+        body: '#28243D',
+        primary: '#F7A701',
+        secondary: '#FEF4CB',
+      },
+    },
+  },
+  plugins: [],
+}
+
